b5dd9b02f9bf1b8b8987fd487a566570379c9fff
Kakjens <Kakjens@users.noreply.github.com>
2017-01-18 22:07:10 +0200

    GCD fix for strenght clases

--- DCSTables.lua
+++ DCSTables.lua
@@ -340,8 +340,11 @@ DCS_TableData.StatData.GCD = {
 				gcd = casterGCD()
 			end
 		else
-			if (primaryStat == LE_UNIT_STAT_INTELLECT) or (classfilename == "HUNTER") or (classfilename == "DEATHKNIGHT") then 
-				-- would be interesting to write "or (primaryStat == LE_UNIT_STAT_STRENGTH)" but needs tests on wariors, paladins
+			if (primaryStat == LE_UNIT_STAT_INTELLECT) or (classfilename == "HUNTER") or (primaryStat == LE_UNIT_STAT_STRENGTH) then 
+				-- adding wariors, paladins
+				-- tested with Crusader Strike, Judgment on retribution paladin
+				-- tested with Consecration, Avenger's Shield, Judgment on protection paladin
+				-- tested with Slam on level 1 warior
 				-- tested with Cobra shot and Multi-shot for hunter. Have troll hunter but don't have pet with Ancient Hysteria //Kakjens
 				-- adding DK-s as reported by Mpstark
 				gcd = casterGCD()
